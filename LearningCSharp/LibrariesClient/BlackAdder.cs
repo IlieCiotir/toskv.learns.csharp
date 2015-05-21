cdde5c31cf073cee39c6542d2672ab8415545ac3
Ilie Ciotir <dipixelhunter@gmail.com>
2015-05-21 23:45:30 +0300

    VS doesn't track BlackAdder? Add it manually.

---
+++ LearningCSharp/LibrariesClient/BlackAdder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Libraries;
+
+namespace LibrariesClient
+{
+    class BlackAdder
+    {
+        static void Main(string[] args)
+        {
+            Baldrick baldrick = new Baldrick();
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Go on Baldrick what's your plan?");
+                Console.WriteLine(baldrick.Plan);
+            }
+                
+        }
+    }
+}
