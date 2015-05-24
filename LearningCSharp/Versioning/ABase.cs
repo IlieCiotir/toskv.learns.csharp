e2923dce2929ea67375eb406a9d62ccaeee1197f
Ilie Ciotir <dipixelhunter@gmail.com>
2015-05-24 22:38:09 +0300

    Next tutorial steps.

---
+++ LearningCSharp/Versioning/ABase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Versioning
+{
+    class ABase
+    {
+        public virtual string AMethod()
+        {
+            return "A method";
+        }
+
+        public string BMethod()
+        {
+            return "B method";
+        }
+    }
+}
