25cf4ad18ebbb5b7475bbadf7ecf28fdc263b38b
Ilie Ciotir <dipixelhunter@gmail.com>
2015-06-01 19:01:33 +0300

    More steps in the tutorial

--- LearningCSharp/IndexedProperties/Program.cs
+++ LearningCSharp/IndexedProperties/Program.cs
@@ -3,9 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace IndexedProperties
 {
+    public class StringIndexer
+    {
+        public class CharacterByteArray
+        {
+
+        }
+        public class TextByteArray
+        {
+            Stream stream;
+
+            public TextByteArray(Stream stream)
+            {
+                this.stream = stream;
+            }
+
+            public byte this[long index]
+            {
+                get
+                {
+                    byte[] buffer = new byte[1];
+                    stream.Seek(index, SeekOrigin.Begin);
+                    stream.Read(buffer, 0, 1);
+                    return buffer[0];
+                }
+
+                set
+                {
+                    stream.Seek(index, SeekOrigin.Begin);
+                    stream.Write(new byte[] { value }, 0, 1);
+                }
+            }
+
+            public long Length
+            {
+                get
+                {
+                    return stream.Seek(0, SeekOrigin.End);
+                }
+            }
+
+            public override string ToString()
+            {
+                return stream.ToString();
+            }
+
+        }
+
+
+        readonly Stream stream;
+
+        public StringIndexer(string input)
+        {
+            stream = new MemoryStream(Encoding.UTF8.GetBytes(input));
+        }
+    }
     class Program
     {
         static void Main(string[] args)
---
+++ LearningCSharp/OperatorOverloading/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    public class Complex
+    {
+        private int real;
+        private int imaginary;
+
+        public Complex(int real, int imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+
+        static public Complex operator +(Complex c1, Complex c2)
+        {
+            return new Complex(c1.real + c1.real, c1.imaginary + c2.imaginary);
+        }
+
+        static public bool operator !=(Complex c1, Complex c2)
+        {
+            return c1.real != c2.real || c1.imaginary != c2.imaginary;
+        }
+
+        static public Boolean operator ==(Complex c1, Complex c2)
+        {
+            return !(c1 != c2);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} + {1}i", real, imaginary);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex(1, 2);
+            Complex c2 = new Complex(3, 3);
+
+            Console.WriteLine("c1 = {0}", c1);
+            Console.WriteLine("c2 = {0}", c2);
+
+            Complex c3 = c1 + c2;
+            Console.WriteLine("c3 = {0}", c3);
+
+            Console.WriteLine("C1 != C3 {0}", c1 != c3);
+        }
+    }
+}
