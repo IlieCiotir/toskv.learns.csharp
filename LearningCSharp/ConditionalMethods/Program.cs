21d5012a1c1ca8fc8be020b93a0118e5ce3769af
Ilie Ciotir <dipixelhunter@gmail.com>
2015-06-01 22:42:34 +0300

    Explicit Interfaces and ConditionalMethods.

---
+++ LearningCSharp/ConditionalMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConditionalMethods
+{
+    public class Log
+    {
+        [Conditional("DEBUG")]
+        public static void DebugMessage(string message)
+        {
+            Console.WriteLine("[TRACE] : {0}", message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.DebugMessage("message");
+            
+        }
+    }
+}
