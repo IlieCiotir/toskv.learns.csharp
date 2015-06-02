105f7a97faf230a6c457c6d65a91bffa5f0d3bf8
Ilie Ciotir <dipixelhunter@gmail.com>
2015-06-02 22:38:43 +0300

    Xml documentation and Platform Invoke steps.

---
+++ LearningCSharp/PlatformInvoke/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace PlatformInvoke
+{
+    class Program
+    {
+        [DllImport("msvcrt.dll")]
+        [return: MarshalAs(UnmanagedType.I4)]
+        public static extern int puts([MarshalAs(UnmanagedType.LPStr)] string c);
+
+        [DllImport("msvcrt.dll")]
+        internal static extern int _flushall();
+
+        static void Main(string[] args)
+        {
+            puts("Let's print some text!");
+            _flushall();
+        }
+    }
+}
---
+++ LearningCSharp/XMLDocumentation/Program.cs
@@ -0,0 +1,64 @@
+﻿/// XmlDocumentation.cs
+/// compile with: /doc:XmlDocumentation.xml
+/// Or just check the box in project properties Build tab.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLDocumentation
+{
+    /// <summary>
+    /// Class level summary documentation goes here.</summary>
+    /// <remarks>
+    /// My remarks are that I like java style of documentation more.</remarks>
+    class Program
+    {
+        /// <summary>
+        /// my pretty member!
+        /// </summary>
+        private string someMember;
+
+        /// <summary>
+        /// Great at doing nothing!
+        /// </summary>
+        public Program()
+        {
+
+        }
+
+        /// <summary>
+        /// Uh look another property
+        /// </summary>
+        /// <value>
+        /// It has a very useful value.
+        /// </value>
+        public string AnotherProperty
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// This is a documented method
+        /// </summary>
+        /// <param name="s">hey this is generated automatically!</param>
+        /// <returns>A number! Somehow related to the input.</returns>
+        /// <seealso cref="String">
+        /// Go look at this too!
+        /// </seealso>
+        public int PrettyMethod(string s)
+        {
+            return s.Length;
+        }
+
+        /// <summary>
+        /// The main method.
+        /// </summary>
+        /// <param name="args">Typy typy!</param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
